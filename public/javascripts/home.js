4e9b610e7343ee23630c90cf83af46ab947051d3
Grant Timmerman <granttimmerman@gmail.com>
2014-04-13 14:43:25 -0700

    Added nav jade template and location finder

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -5,4 +5,15 @@ $(function() {
       window.location.href = '/search/' + query;
     }
   });
+
+  function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+    } else{x.innerHTML = "Geolocation is not supported by this browser.";}
+  }
+  function showPosition(position) {
+    var coord = position.coords;
+    console.log(coord);
+  }
+  getLocation();
 });
